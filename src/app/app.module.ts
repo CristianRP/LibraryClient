0319fdf71cbf024ba1a89a774460eb16497c92ed
Cristian Ramirez <cristianramirezgt@gmail.com>
2017-10-06 15:53:00 -0600

    First commit, customers module completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { CustomersComponent } from './customers/customers.component';
+import { SearchCustomersComponent } from './search-customers/search-customers.component';
+import { DataService } from './data.service';
+
+import { enableProdMode } from '@angular/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerDetailsComponent,
+    CustomersComponent,
+    CreateCustomerComponent,
+    SearchCustomersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/customers/customer.ts
@@ -0,0 +1,8 @@
+export class Customer {
+  public id: number;
+  public firstName: string;
+  public lastName: string;
+  public carne: string;
+  public userName: string;
+  public password: string;
+}
